e632790c7c28c104f98a58deb0721d143ed87e1b
medo94my <medoroyalrma@gmail.com>
2020-01-29 18:46:15 +0800

    servo-motor

--- flask-test/smart-home/app.py
+++ flask-test/smart-home/app.py
@@ -4,10 +4,13 @@ import time
 import picamera
 app=Flask(__name__)
 counter =0
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-LED = 27
-GPIO.setup(LED,GPIO.OUT)
+#LED = 3
+#GPIO.setup(LED,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)             
+p = GPIO.PWM(3,50)
+p.start(7.5) 
 # GPIO.output(LED,GPIO.OUT)
 # ledstats=GPIO.LOW
 @app.route('/')
@@ -38,20 +41,19 @@ def pic():
 
     return 'camera'
 @app.route('/ac-on')
-def ac_on():
-    for count in range(2):
-        GPIO.output(LED,GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(LED,GPIO.LOW)
-        time.sleep(.5)
+def ac_on():                           
+    while 1:                                                                        
+            p.ChangeDutyCycle(7.5)              
+            time.sleep(1)                      
+            p.ChangeDutyCycle(12.5)            
+            time.sleep(1)                   
+            p.ChangeDutyCycle(2.5)          
+            time.sleep(1)                   
+
     return 'ac on'
 @app.route('/ac-off')
 def ac_off():
-    for count in range(2):
-        GPIO.output(LED,GPIO.LOW)
-        time.sleep(2)
-        GPIO.output(LED,GPIO.HIGH)
-        time.sleep(1)
+    p.ChangeDutyCycle(2.5)
     return 'ac-of'
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
